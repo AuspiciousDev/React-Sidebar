769562c6aff54290c24be1adb487536cf7f79b5e
Auspicious <kingmcchester.tabing.ds@bulsu.edu.ph>
2024-03-29 23:17:28 +0800

    Update Sidebar.jsx

--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
   const [open, setOpen] = useState(false)
@@ -8,13 +9,15 @@ const Sidebar = () => {
     setOpen(!open)
   }
   return (
-    <div className={`flex ${open ? "w-[300px]" : "w-[50px]"} bg-gray-300  transition-all duration-400`}>
+    <div className={`flex flex-col ${open ? "w-[300px]" : "w-[50px]"} bg-gray-300  transition-all duration-400`}>
       <div className='h-[50px]'>
         <button className="border border-cyan-50 " onClick={handleSidebar}>=</button>
       </div>
-      {
-        open ? <span>Open</span> : <span>Closed</span>
-      }
+      <div className='flex flex-col gap-2 p-2'>
+        <Link to='/'>Home</Link>
+        <div className='w[50%] border-b border-gray-400' />
+        <Link to='/profile'>Profile</Link>
+      </div>
     </div>
   )
 }
